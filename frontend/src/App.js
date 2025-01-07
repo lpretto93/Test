7ccb1503ea665ecbe6e45a60ebddce730b13bb2d
lpretto93 <lpretto@hotmail.it>
2025-01-07 13:27:19 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,17 @@
 
+import React from "react";
+import "./styles/App.css";
+import TeamManager from "./components/TeamManager";
+import MatchSimulator from "./components/MatchSimulator";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Game Manager</h1>
+      <TeamManager />
+      <MatchSimulator />
+    </div>
+  );
+}
+
+export default App;
